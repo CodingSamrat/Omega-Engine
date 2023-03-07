87075371f7bd9d6da1ac57c93a7bdac6a7661a96
Sam <samratbiswas986@gmail.com>
2023-03-07 18:42:39 +0530

    Created Core [lib]

---
+++ Core/include/core/core.cpp
@@ -0,0 +1,10 @@
+// Core.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+
+// TODO: This is an example of a library function
+void Test()
+{
+	std::cout << "Working...\n\t[OK] 200";
+}
---
+++ Core/include/core/pch.h
@@ -0,0 +1,10 @@
+
+#ifndef PCH_H
+#define PCH_H
+
+// add headers that you want to pre-compile here
+#include <iostream>
+
+
+
+#endif //PCH_H
